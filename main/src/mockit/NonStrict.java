9951aee351c5a0cf54fe5c668d60e9a688729a09
javamockit@yahoo.com <javamockit@yahoo.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2009-10-24 22:09:20 +0000

    Fixed a mistake in the documentation of the annotation.
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@110 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

--- main/src/mockit/NonStrict.java
+++ main/src/mockit/NonStrict.java
@@ -40,16 +40,17 @@ import java.lang.annotation.*;
  * Note that multiple invocations to the same method or constructor can be recorded, provided
  * different arguments are used.
  * <p/>
- * Each recorded invocation still specifies an expectation, so a corresponding invocation in the
- * replay phase will be required for the test to pass.
- * By default, more than one such corresponding invocations can occur for each recorded invocation
- * (in any order).
- * If an upper limit or exact number of invocations is desired, then the appropriate constraint
- * method should be used in the record phase.
+ * Each recorded non-strict invocation still specifies an expectation, but without any invocation
+ * count constraint to be satisfied. Therefore, a corresponding invocation in the replay phase will
+ * not be required for the test to pass.
+ * By default, any number of such corresponding invocations can occur in the replay phase for the
+ * recorded invocation, in any order.
+ * If a lower/upper limit or an exact number of invocations is expected, then the appropriate
+ * constraint method should be used after recording the invocation.
  * <p/>
- * Invocations that are <strong>not</strong> recorded but should occur in the replay phase may be
- * explicitly verified at the end of the test using a {@linkplain mockit.Verifications
- * verification block}.
+ * Invocations to non-strict mocks or mocked types that should occur in the replay phase can be
+ * explicitly verified at the end of the test using a {@linkplain mockit.Verifications verification
+ * block}.
  * <p/>
  * <a href="http://jmockit.googlecode.com/svn/trunk/www/tutorial/BehaviorBasedTesting.html#strictness">Tutorial</a>
  */
