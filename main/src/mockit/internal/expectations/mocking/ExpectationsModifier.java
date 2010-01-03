c17034d45a0e6931b9db7bac191f211cbb4485b9
javamockit@yahoo.com <javamockit@yahoo.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2010-01-03 21:46:54 +0000

    Updated file header.
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@804 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

--- main/src/mockit/internal/expectations/mocking/ExpectationsModifier.java
+++ main/src/mockit/internal/expectations/mocking/ExpectationsModifier.java
@@ -1,6 +1,6 @@
 /*
  * JMockit Expectations & Verifications
- * Copyright (c) 2006-2009 Rogério Liesenfeld
+ * Copyright (c) 2006-2010 Rogério Liesenfeld
  * All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
