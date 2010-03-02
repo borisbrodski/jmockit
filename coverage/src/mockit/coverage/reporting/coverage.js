d9bed3716d92af090c1dc93f4ca565550746dac6
javamockit@yahoo.com <javamockit@yahoo.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2010-03-02 15:42:20 +0000

    Fixed an issue with lines for unknown source files.
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@995 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

--- coverage/src/mockit/coverage/reporting/coverage.js
+++ coverage/src/mockit/coverage/reporting/coverage.js
@@ -78,38 +78,54 @@ function showHideLines(row)
 var metricCol;
 function rowOrder(r1, r2)
 {
-  var t1 = r1.cells[metricCol].title;
-  var t2 = r2.cells[metricCol].title;
+   var c1 = r1.cells[metricCol];
+   var c2 = r2.cells[metricCol];
 
-  if (t1 && t2) {
-    var c1 = t1.split('/')[1];
-    var c2 = t2.split('/')[1];
-    return c1 - c2;
-  }
+   if (!c1 || !c2) {
+      return -1;
+   }
 
-  return t1 ? 1 : -1;
+   var t1 = c1.title;
+   var t2 = c2.title;
+
+   if (t1 && t2) {
+      var s1 = t1.split('/')[1];
+      var s2 = t2.split('/')[1];
+      return s1 - s2;
+   }
+
+   return t1 ? 1 : -1;
 }
 
 function sortRows(tbl, metric)
 {
-  var colCount = tbl.rows[0].cells.length;
-  var startIndex = colCount == 5 ? 1 : 0;
-  var rs = new Array();
+   var startRow = 0;
+   var endRow = tbl.rows.length;
 
-  for (var i = startIndex; i < tbl.rows.length; i++) {
-    rs[i - startIndex] = tbl.rows[i];
-  }
+   if (tbl.rows[0].cells.length == 5) {
+      metricCol = 1 + metric;
+      startRow = 1;
+      endRow--;
+   }
+   else {
+      metricCol = metric;
+   }
 
-  metricCol = colCount - 4 + metric;
-  rs.sort(rowOrder);
+   var rs = new Array();
 
-  for (var i = 0; i < rs.length; i++) {
-    rs[i] = rs[i].innerHTML;
-  }
+   for (var i = startRow; i < endRow; i++) {
+      rs[i - startRow] = tbl.rows[i];
+   }
 
-  for (var i = 0; i < rs.length; i++) {
-    tbl.rows[startIndex + i].innerHTML = rs[i];
-  }
+   rs.sort(rowOrder);
+
+   for (var i = 0; i < rs.length; i++) {
+      rs[i] = rs[i].innerHTML;
+   }
+
+   for (var i = 0; i < rs.length; i++) {
+      tbl.rows[startRow + i].innerHTML = rs[i];
+   }
 }
 
 function sortTables(metric)
