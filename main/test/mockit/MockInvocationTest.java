2c04b19426bff141633acb0e3d03146c54e526d2
javamockit@yahoo.com <javamockit@yahoo.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2010-04-19 16:53:08 +0000

    Initial tests for new feature: Invocation parameter in @Mock methods (issue 16).
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@1094 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

---
+++ main/test/mockit/MockInvocationTest.java
@@ -0,0 +1,97 @@
+/*
+ * JMockit Annotations
+ * Copyright (c) 2010 Rogério Liesenfeld
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package mockit;
+
+import java.util.*;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public final class MockInvocationTest
+{
+   static class Collaborator
+   {
+      int value;
+      
+      Collaborator() {}
+      Collaborator(int i) { value = i; }
+
+      int getValue() { return -1; }
+      String doSomething(boolean b, int[] i, String s) { return s + b + i[0]; }
+      static boolean staticMethod() { return true; }
+      static boolean staticMethod(int i) { return i > 0; }
+      native long nativeMethod(boolean b);
+      final char finalMethod() { return 's'; }
+      private float privateMethod() { return 1.2F; }
+      void addElements(Collection<String> elements) { elements.add("one element"); }
+   }
+
+   @Ignore @Test
+   public void mockMethodWithContextObject()
+   {
+      new MockUp<Collaborator>()
+      {
+         @Mock
+         boolean staticMethod(Invocation context)
+         {
+            assertTrue(context.getInvokedInstance() instanceof Collaborator);
+            assertEquals(context.getInvocationCount() - 1, context.getInvocationIndex());
+            assertEquals(0, context.getMinInvocations());
+            assertEquals(-1, context.getMaxInvocations());
+            return context.getInvocationCount() <= 0;
+         }
+      };
+
+      assertFalse(Collaborator.staticMethod());
+      assertFalse(Collaborator.staticMethod());
+   }
+
+   @MockClass(realClass = Collaborator.class)
+   static class MockForConstructor
+   {
+      int capturedArgument;
+
+      @Mock(invocations = 1)
+      void $init(Invocation context, int i)
+      {
+         assertTrue(context.getInvokedInstance() instanceof Collaborator);
+         assertEquals(1, context.getMinInvocations());
+         assertEquals(1, context.getMaxInvocations());
+         capturedArgument = i + context.getInvocationCount();
+      }
+   }
+
+//   @Test // TODO: when this test fails, it seems to cause later tests to fail
+   public void mockForConstructorWithContext()
+   {
+      MockForConstructor mock = new MockForConstructor();
+      Mockit.setUpMocks(MockForConstructor.class);
+
+      new Collaborator(4);
+
+      assertEquals(5, mock.capturedArgument);
+   }
+}
