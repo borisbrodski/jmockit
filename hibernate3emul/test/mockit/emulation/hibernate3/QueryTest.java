8af0a1dd21ee9fa95196a9d7e77fc6efd56c04f3
javamockit@yahoo.com <javamockit@yahoo.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2010-12-05 00:35:56 +0000

    Added missing source file headers.
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@1291 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

--- hibernate3emul/test/mockit/emulation/hibernate3/QueryTest.java
+++ hibernate3emul/test/mockit/emulation/hibernate3/QueryTest.java
@@ -1,3 +1,27 @@
+/*
+ * JMockit Hibernate 3 Emulation
+ * Copyright (c) 2006-2010 Rogério Liesenfeld
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package mockit.emulation.hibernate3;
 
 import java.math.*;
@@ -252,8 +276,7 @@ public final class QueryTest
    public void findChildrenEntitiesWithGivenParentName()
    {
       addChildrenEntities();
-      QueryEmul query =
-         new QueryEmul("select c from ChildEntity c where c.parent.name like ?", entities);
+      QueryEmul query = new QueryEmul("select c from ChildEntity c where c.parent.name like ?", entities);
       query.setParameter(0, "%y%");
 
       ScrollableResults found = query.scroll();
@@ -266,14 +289,12 @@ public final class QueryTest
    public void findChildrenAndParentEntitiesWithGivenParentSeqNumber()
    {
       addChildrenEntities();
-      QueryEmul query =
-         new QueryEmul(
-            "select c, p from ChildEntity c join c.parent p where p.seqNumber=3", entities);
+      QueryEmul query = new QueryEmul("select c, p from ChildEntity c join c.parent p where p.seqNumber=3", entities);
 
-      List<Object[]> found = (List<Object[]>) query.list();
+      List<?> found = query.list();
 
       assertEquals(1, found.size());
-      Object[] result = found.get(0);
+      Object[] result = (Object[]) found.get(0);
       assertSame(child, result[0]);
       assertSame(child.getParent(), result[1]);
    }
