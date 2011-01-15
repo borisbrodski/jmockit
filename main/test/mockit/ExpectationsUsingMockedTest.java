df47bb81b5440f92e4b422d20d515cf534df0b5e
javamockit@yahoo.com <javamockit@yahoo.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2011-01-15 18:07:03 +0000

    Added test which mocks an inner class.
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@1413 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

--- main/test/mockit/ExpectationsUsingMockedTest.java
+++ main/test/mockit/ExpectationsUsingMockedTest.java
@@ -1,26 +1,6 @@
 /*
- * JMockit Expectations
- * Copyright (c) 2006-2010 Rogério Liesenfeld
- * All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2006-2011 Rogério Liesenfeld
+ * This file is subject to the terms of the MIT license (see LICENSE.txt).
  */
 package mockit;
 
@@ -290,5 +270,25 @@ public final class ExpectationsUsingMockedTest
    {
       assertEquals(0, AnotherClassWithStaticInitializer2.initialized());
       assertTrue(AnotherClassWithStaticInitializer2.initialized);
+   }
+
+   class InnerClass
+   {
+      int getValue() { return -1; }
+   }
+
+   @Test
+   public void mockInnerClass(final InnerClass innerMock)
+   {
+      assert innerMock.getValue() == 0;
+
+      new NonStrictExpectations()
+      {
+         {
+            innerMock.getValue(); result = 123; times = 1;
+         }
+      };
+
+      assert new InnerClass().getValue() == 123;
    }
 }
