19710ea24519fc693a0b446c5fbdee55e7b80abc
rliesenfeld@gmail.com <rliesenfeld@gmail.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2012-09-04 16:57:19 +0000

    Fixed some inaccuracies in the API documentation of the Delegate interface.
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@2111 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

--- main/src/mockit/Delegate.java
+++ main/src/mockit/Delegate.java
@@ -1,30 +1,43 @@
 /*
- * Copyright (c) 2006-2011 Rogério Liesenfeld
+ * Copyright (c) 2006-2012 Rogério Liesenfeld
  * This file is subject to the terms of the MIT license (see LICENSE.txt).
  */
 package mockit;
 
 /**
- * An empty interface to be used with the {@link Expectations#result} field or the {@link Invocations#with(Delegate)}
- * method, allowing test code to define invocation results (return values or thrown exceptions) or argument matching
- * rules based on test-specific logic.
+ * An empty interface to be used with the {@link Expectations#result} field or the
+ * <a href="Expectations.html#with(mockit.Delegate)">{@code Invocations#with(Delegate)}</a> method, allowing test code
+ * to define invocation results or argument matching rules based on test-specific logic.
  * <p/>
- * When used with the {@code result} field, the following rules apply to the implementation class:
- * <ol>
- * <li>A method matching the signature of the recorded method/constructor call must be defined.
- * That is, they should have the same name and parameters. In the case of delegating a constructor, a delegate
- * <em>method</em> should still be used, with name "$init".</li>
- * <li>Alternatively, for the common case where the delegate implementation class defines a <em>single</em> method, the
- * name is allowed to be different from the recorded method.
- * The parameters should still match, though. Besides giving more flexibility, this ability also prevents the test from
- * breaking in case the recorded method is renamed.
- * If the delegate class defines two or more methods (including {@code private} ones, if any), then exactly one
- * of them <em>must</em> match both the name and the parameters of the recorded method.</li>
- * <li>The result of a delegate method execution can be any return value compatible with the recorded method's return
- * type, or a thrown error/exception.</li>
- * </ol>
- * When used with the {@code with} method, the implementing class must have a single non-{@code private} method which
- * returns a {@code boolean}, being {@code true} for a succesfully matched argument or {@code false} otherwise.
+ * The name and parameters of the delegate method must fit one of the following alternatives:
+ * <ul>
+ * <li>
+ *    A method matching the signature of the recorded method/constructor.
+ *    That is, they should have the same name and parameters. In the case of delegating a constructor, a delegate
+ *    <em>method</em> should still be used, with name "$init".
+ *    In this case, the delegate class can have any number of additional methods.
+ * </li>
+ * <li>
+ *    A <em>single</em> method with the same or a different name as the recorded method, but having the <em>same</em>
+ *    parameters.
+ * </li>
+ * <li>
+ *    A <em>single</em> method with the same or a different name, but having <em>no</em> parameters even if the recorded
+ *    method/constructor has one or more of them.
+ *    In this case, argument values passed during replay are simply ignored as far as the delegate is concerned.
+ * </li>
+ * </ul>
+ * Additionally, in any of the above three alternatives, the delegate method is allowed to declare an additional
+ * parameter of type {@link Invocation}.
+ * If said parameter is present, it must be the first one.
+ * At replay time, it will always receive a suitable object representing the associated invocation which happened to
+ * match the recorded expectation.
+ * <p/>
+ * When used with the {@code result} field, the result of a delegate method execution can be any return value compatible
+ * with the recorded method's return type, or a thrown error/exception.
+ * <p/>
+ * When used with the {@code with} method, the delegate method must return a {@code boolean}, being {@code true} for a
+ * successfully matched argument or {@code false} otherwise.
  * <p/>
  * At replay time, when the mocked method/constructor is called the corresponding "delegate" method will be called.
  * The arguments passed to the delegate method will be the same as those received by the recorded invocation during
@@ -32,7 +45,7 @@ package mockit;
  * Even {@code static} methods in the mocked type can have delegates, which in turn can be static or not.
  * The same is true for {@code private}, {@code final}, and {@code native} methods.
  * <p/>
- * <a href="http://jmockit.googlecode.com/svn/trunk/www/tutorial/BehaviorBasedTesting.html#delegates">In the Tutorial</a>
+ * <a href="../../tutorial/BehaviorBasedTesting.html#delegates">In the Tutorial</a>
  * <p/>
  * Sample tests:
  * <a href="http://code.google.com/p/jmockit/source/browse/trunk/samples/easymock/test/org/easymock/samples/DocumentManager_JMockit_Test.java">DocumentManager_JMockit_Test</a>
