52d78d7d9115d0e7e4d9b85783e2eea743f0051f
rliesenfeld@gmail.com <rliesenfeld@gmail.com@4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8>
2012-12-12 10:18:38 +0000

    Removed temporary "@Ignore" from test.
    git-svn-id: http://jmockit.googlecode.com/svn/trunk@2260 4d38ed0c-b8d3-11de-a71d-8f2b2e058ad8

--- main/test/mockit/ClassLoadingAndJREMocksTest.java
+++ main/test/mockit/ClassLoadingAndJREMocksTest.java
@@ -337,7 +337,7 @@ public final class ClassLoadingAndJREMocksTest
       return mainClassName;
    }
 
-   @Ignore("failing only in the Maven run") @Test
+   @Test
    public void mockJavaUtilJarClasses() throws Exception
    {
       final File testFile = new File("test.jar");
